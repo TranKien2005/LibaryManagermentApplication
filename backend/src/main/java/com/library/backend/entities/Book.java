833112d35735b67ca4818a6941ae2546eb37b5e2
Nguyen Minh Quan <minhquan15032005@gmail.com>
2025-10-16 00:42:08 +0700

    fix

--- backend/src/main/java/com/library/backend/entities/Book.java
+++ backend/src/main/java/com/library/backend/entities/Book.java
@@ -27,15 +27,12 @@ public class Book {
 
     Integer yearPublished;
 
-    @Column(nullable = false)
     Integer availableCopies = 0;
 
     String description;
 
-    @Column(nullable = false)
     Double rating = 0.0;
 
-    @Column(nullable = false)
     Integer reviewCount = 0;
 
     String coverImageUrl;
